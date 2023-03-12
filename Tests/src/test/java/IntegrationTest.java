3f92498f0c8dcc7c68d5976f37be8ff291416ad6
Kamil Graczyk <kamillokoks@gmail.com>
2023-03-12 01:38:56 +0100

    Testcontainers now use payara full server. Created requests specs to test with different auths.

--- Tests/src/test/java/IntegrationTest.java
+++ Tests/src/test/java/IntegrationTest.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
+import io.restassured.specification.RequestSpecification;
+import jakarta.ws.rs.core.Response.Status;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
@@ -10,16 +13,24 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
+import pl.lodz.p.it.tks.model.Room;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.UUID;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 
 
 @Testcontainers
 public class IntegrationTest {
+
+    private static RequestSpecification clientSpec;
+    private static RequestSpecification employeeSpec;
+    private static RequestSpecification adminSpec;
 
     @BeforeAll
     public static void setup() {
@@ -41,13 +52,12 @@ public class IntegrationTest {
                 .withNetwork(network)
                 .withNetworkAliases("db");
 
-        GenericContainer payaraContainer = new GenericContainer("payara/micro:6.2023.2-jdk17")
+        GenericContainer payaraContainer = new GenericContainer("payara/server-full:6.2023.2-jdk17")
                 .withExposedPorts(8181)
                 .dependsOn(postgresContainer)
                 .withNetwork(network)
                 .withNetworkAliases("payara")
                 .withCopyFileToContainer(warFile, "/opt/payara/deployments/RestController-1.0.war")
-                .withCommand("--deploy /opt/payara/deployments/RestController-1.0.war --autoBindSsl")
                 .waitingFor(Wait.forHttps("/api/rooms").allowInsecure());
 
         postgresContainer.start();
@@ -56,14 +66,96 @@ public class IntegrationTest {
         RestAssured.baseURI = "https://localhost/";
         RestAssured.port = payaraContainer.getMappedPort(8181);
         RestAssured.useRelaxedHTTPSValidation();
+
+        generateClientSpec();
+        generateEmployeeSpec();
+        generateAdminSpec();
     }
+
+    static void generateClientSpec() {
+        JSONObject clientCredentials = new JSONObject();
+        clientCredentials.put("username", "client");
+        clientCredentials.put("password", "password");
+
+        String clientJwt = given().body(clientCredentials.toString())
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/login")
+                .jsonPath()
+                .get("jwt");
+
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+
+        builder.addHeader("Authorization", "Bearer " + clientJwt);
+        clientSpec = builder.build();
+    }
+
+    static void generateEmployeeSpec() {
+        JSONObject employeeCredentials = new JSONObject();
+        employeeCredentials.put("username", "employee");
+        employeeCredentials.put("password", "password");
+
+        String employeeJwt = given().body(employeeCredentials.toString())
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/login")
+                .jsonPath()
+                .get("jwt");
+
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+
+        builder.addHeader("Authorization", "Bearer " + employeeJwt);
+        employeeSpec = builder.build();
+    }
+
+    static void generateAdminSpec() {
+        JSONObject adminCredentials = new JSONObject();
+        adminCredentials.put("username", "admin");
+        adminCredentials.put("password", "password");
+
+        String adminJwt = given().body(adminCredentials.toString())
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/login")
+                .jsonPath()
+                .get("jwt");
+
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+
+        builder.addHeader("Authorization", "Bearer " + adminJwt);
+        adminSpec = builder.build();
+    }
+
+
 
     @Test
     void shouldReturnListOfRoomsWithStatusCode200() {
         when().get("/api/rooms")
                 .then()
-                .assertThat().statusCode(HttpStatus.SC_OK)
+                .assertThat().statusCode(Status.OK.getStatusCode())
                 .assertThat().contentType(ContentType.JSON);
     }
 
+    @Test
+    void shouldCreateRoomWithStatusCode201() {
+
+        Room room = new Room(1, 600.0, 1);
+
+        JSONObject req = new JSONObject(room);
+        UUID id = given().spec(employeeSpec).contentType(ContentType.JSON)
+                .body(req.toString())
+                .when()
+                .post("/api/rooms")
+                .then()
+                .statusCode(Status.CREATED.getStatusCode())
+                .extract().jsonPath().getUUID("id");
+
+        given().spec(employeeSpec).when().get("api/rooms/" + id)
+                .then()
+                .statusCode(Status.OK.getStatusCode())
+                .contentType(ContentType.JSON)
+                .body("id", equalTo(id.toString()),
+                        "price", equalTo(600.0f),
+                        "size", equalTo(1));
+    }
 }
