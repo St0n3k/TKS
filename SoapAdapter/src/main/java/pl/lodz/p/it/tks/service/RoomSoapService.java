59200295c7fe6e2e1c1572e810a3afcb4dee8c10
Kamil Graczyk <49203388+St0n3k@users.noreply.github.com>
2023-03-20 20:11:59 +0100

    Fixed SOAP Service deployment while deploying merged WARs.

--- SoapAdapter/src/main/java/pl/lodz/p/it/tks/service/RoomSoapService.java
+++ SoapAdapter/src/main/java/pl/lodz/p/it/tks/service/RoomSoapService.java
@@ -1,26 +1,13 @@
 package pl.lodz.p.it.tks.service;
 
-import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 import pl.lodz.p.it.tks.model.RoomSoapDTO;
-import pl.lodz.p.it.tks.ui.query.RoomQueryUseCase;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@WebService(serviceName = "RoomAPI")
-public class RoomSoapService {
-
-    @Inject
-    private RoomQueryUseCase roomQueryUseCase;
-
-
+@WebService
+public interface RoomSoapService {
     @WebMethod
-    public List<RoomSoapDTO> getAllRooms() {
-        return roomQueryUseCase.getAllRooms()
-                .stream().map(RoomSoapDTO::new)
-                .collect(Collectors.toList());
-    }
-
+    List<RoomSoapDTO> getAllRooms();
 }
