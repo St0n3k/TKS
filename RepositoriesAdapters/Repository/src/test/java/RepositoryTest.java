5d1f87e2c25e85353da708c3560f92a115b6d2f0
rstrzalkowski <resumc@wp.pl>
2023-03-24 12:58:24 +0100

    Arquillian configuration for repository tests using already running postgresql

---
+++ RepositoriesAdapters/Repository/src/test/java/RepositoryTest.java
@@ -0,0 +1,48 @@
+import jakarta.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import pl.lodz.p.it.tks.model.AbstractEntity;
+import pl.lodz.p.it.tks.model.RoomEntity;
+import pl.lodz.p.it.tks.model.user.UserEntity;
+import pl.lodz.p.it.tks.repository.RoomRepository;
+import pl.lodz.p.it.tks.repository.impl.RoomRepositoryImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(ArquillianExtension.class)
+public class RepositoryTest {
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackage(RoomRepository.class.getPackage())
+                .addPackage(RoomRepositoryImpl.class.getPackage())
+                .addPackage(AbstractEntity.class.getPackage())
+                .addPackage(UserEntity.class.getPackage())
+                .addAsLibraries("postgresql-42.5.4.jar")
+                .addAsResource("initial_data.sql", "META-INF/sql/initial_data.sql")
+                .addAsResource("persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+
+        return war;
+    }
+
+    @Inject
+    private RoomRepository roomRepository;
+
+    @Test
+    void getRoomByNumberTest() {
+        RoomEntity roomEntity = roomRepository.getByRoomNumber(836)
+                .orElseThrow();
+
+        assertEquals(1, roomEntity.getSize());
+        assertEquals(836, roomEntity.getRoomNumber());
+        assertEquals(707.19, roomEntity.getPrice());
+    }
+}
